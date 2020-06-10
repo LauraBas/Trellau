9d12c70e96b1038edc84e18a76db13dbafb851d2
Laura Bassani <bassanilaurarita@gmail.com>
2020-06-10 20:49:07 +0200

    add title to new tasks

--- src/AddTasks.js
+++ src/AddTasks.js
@@ -8,7 +8,7 @@ class AddTasks extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            input:''
+            input: ''
         }
         this.add = this.add.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -16,29 +16,28 @@ class AddTasks extends React.Component {
 
     add(event) {
         event.preventDefault();
-        const str = event.target.value;
-        this.props.addTask(str)
+        this.props.addTask(this.state.input)
+        this.setState({input: ""})
     }
 
     handleChange(event) {
-    this.setState({
-      input: event.target.value
-    });
-}
+        this.setState({
+            input: event.target.value
+        });
+    }
 
     render() {
         return (
             <div>
                 <div className="container">
                     <p className="add">Add your task here</p>
-                    <input onChange={this.handleChange} className="input"/>
+                    <input value={this.state.input} onChange={this.handleChange} className="input"/>
                     <p className="importance">Level of importance</p>
                     <button className="btn" id="red">High</button>
                     <button className="btn" id="yellow">Middle</button>
                     <button className="btn" id="green">Low</button>
                     <button onClick={this.add} id="go">Add <i className="fa fa-thumbs-up"></i></button>
                 </div>
-
             </div>
         );
     }
