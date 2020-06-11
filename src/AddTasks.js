19af09a349a73780aea029cdb86039fbc0755cf7
Laura Bassani <bassanilaurarita@gmail.com>
2020-06-11 19:14:19 +0200

    add keypress event to add tasks

--- src/AddTasks.js
+++ src/AddTasks.js
@@ -12,6 +12,7 @@ class AddTasks extends React.Component {
         }
         this.add = this.add.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this)
     }
 
     add(event) {
@@ -26,14 +27,22 @@ class AddTasks extends React.Component {
         });
     }
 
+    handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            this.props.addTask(this.state.input)
+            this.setState({input: ""})
+        }
+    }
 
     render() {
         return (
             <div>
                 <div className="container">
                     <p className="add">Add your task here</p>
-                    <input value={this.state.input} onChange={this.handleChange} className="input"/>
-                    <button onClick={this.add} id="go">Add <i className="fa fa-thumbs-up"></i></button>
+                    <input onKeyPress={this.handleKeyPress} value={this.state.input} onChange={this.handleChange}
+                           className="input"/>
+                    <button onClick={this.add} id="go">Add <i
+                        className="fa fa-thumbs-up"></i></button>
                 </div>
             </div>
         );
